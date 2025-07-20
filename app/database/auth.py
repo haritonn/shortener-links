b3998bae6ab99f532e2fa580efbb2227fde2021d
Hariton <pitsikhariton@gmail.com>
2025-07-21 03:24:04 +0400

    fix: moving db routes to flask_sqlalchemy

--- app/database/auth.py
+++ app/database/auth.py
@@ -1,40 +1,35 @@
-from db import Session, User
+from app.database.db import db, User
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # returns false if username is not found
 def login_username(user):
-    session = Session()
-    results = session.query(User).filter(User.name==user).all()
-    if len(results) == 0:
-        session.close()
-        return False 
-    
-    session.close()
-    return True 
+    query = User.query.filter_by(User.username==user).first()
+    if query is None:
+        return True 
+
+    return False
+
 
 # check if password correct (should be used after checking username)
 def login_password(user, password):
-    session = Session()
-    results = session.query(User).filter(User.name == user).all()
+    query = User.query.filter_by(User.username==user).first()
+    if not query:
+        return False 
     
-    if check_password_hash(results[0], password):
-        session.close()
-        return True 
-    
-    session.close()
-    return False 
+    return check_password_hash(query.password, password)
 
 # registration of new user (True -> redirect to application page, False -> this user already exists)
 def registration_user(user, password, error=None):
-    new_user = User(username=user, password=generate_password_hash(password))
+    new_user = User(username=user, password=generate_password_hash(password)) 
+    
     try:
-        with Session() as session:
-            session.add(new_user)
-            session.commit()
-        
+
+        db.session.add(new_user)
+        db.session.commit()
+
     except IntegrityError:
-        session.rollback()
+        db.session.rollback()
         error = f'User {user} already exists'
     
     return error
